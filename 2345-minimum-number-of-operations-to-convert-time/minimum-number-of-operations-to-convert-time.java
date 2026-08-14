a684d5c7510ac7f95afb27992df1fb6ee7d6b6d1
Rakuram1707 <151542507+Rakuram1707@users.noreply.github.com>
2026-08-14 21:10:20 +0530

    Time: 1 ms (87.58%) | Memory: 43.1 MB (75.82%) - LeetSync

--- 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.java
+++ 2345-minimum-number-of-operations-to-convert-time/minimum-number-of-operations-to-convert-time.java
@@ -1,40 +1,26 @@
 class Solution {
     public int convertTime(String current, String correct) {
 
-        String[] p = current.split(":");
-        String[] p1 = correct.split(":");
+        String[] a = current.split(":");
+        String[] b =correct.split(":");
 
-        String h1 = p[0];
-        String h2 = p1[0];
-        String m1 = p[1];
-        String m2 = p1[1];
+        int start = Integer.parseInt(a[0]) * 60 + Integer.parseInt(a[1]);
+        int end =Integer.parseInt(b[0]) * 60 + Integer.parseInt(b[1]);
+        int diff=end - start;
 
-        int tot1 = Integer.parseInt(h1) * 60 + Integer.parseInt(m1);
-        int tot2 = Integer.parseInt(h2) * 60 + Integer.parseInt(m2);
+        int[] ops = {60, 15, 5, 1};
+        int i = 0;
+        int count = 0;
+        while (diff > 0) {
 
-        int d =tot2 -tot1;
-        int c =0;
-
-        while (d != 0) {
-
-            if (d >= 60) {
-                c+=d / 60;
-                d=d % 60;
-            }
-            else if (d >= 15) {
-                c+= d / 15;
-                d=d % 15;
-            }
-            else if (d >= 5) {
-                c += d/ 5;
-                d =d %5;
-            }
-            else {
-                c += d;
-                d =0;
+            if (diff >= ops[i]) {
+                diff -= ops[i];
+                count++;
+            } else {
+                i++;
             }
         }
 
-        return c;
+        return count;
     }
 }
